ee10fb37f9d06eb246fd6d12d555cc66e083fd6f
harmankaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2025-01-23 22:33:42 +0530

    Create Product_of_Array_Except_Self.py

---
+++ array_and_strings/Product_of_Array_Except_Self.py
@@ -0,0 +1,23 @@
+from functools import reduce
+from operator import mul
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = []
+        l_mul = 1
+        r_mul = 1
+        length = len(nums)
+        l_arr = [1] * length
+        r_arr = [1] * length
+        for i in range(length):
+            # opposite index from right
+            j = - i - 1
+            l_arr[i] = l_mul
+            l_mul *= nums[i]
+            r_arr[j] = r_mul
+            r_mul *= nums[j]
+        
+        # final_output
+        for index in range(length):
+            ans.append(l_arr[index] * r_arr[index])
+        return ans
