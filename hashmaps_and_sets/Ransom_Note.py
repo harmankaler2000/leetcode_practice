550683cbea87786279032e76f3f4fd6972c09678
harmankaler2000 <45006546+harmankaler2000@users.noreply.github.com>
2025-01-27 01:00:42 +0530

    Optimised Solution

--- hashmaps_and_sets/Ransom_Note.py
+++ hashmaps_and_sets/Ransom_Note.py
@@ -1,14 +1,10 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         # create a mapping of the letters in magazine
-        map_mag = {}
-        for letter in set(magazine):
-            map_mag[letter] = magazine.count(letter)
-        
         # map for ransom note
         for letter in set(ransomNote):
-            if letter not in map_mag.keys():
+            if letter not in magazine:
                 return False
-            if ransomNote.count(letter) > map_mag[letter]:
+            if ransomNote.count(letter) > magazine.count(letter):
                 return False
         return True
